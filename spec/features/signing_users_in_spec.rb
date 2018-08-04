5634617e02a6b80cdabc88a98c9be3fcdcf9a97b
Bhagath <bhagath@sproutssolutions.com>
2018-08-04 14:41:52 +0530

    add user sign in feature

---
+++ spec/features/signing_users_in_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.feature "Users signin" do
+
+before do
+  @bhagath = User.create!(email: "bhagath@gmail.com",
+			                    password: "password")
+end
+
+scenario "with valid credentials" do
+
+visit "/"
+
+click_link "Sign in"
+
+fill_in "Email", with: @bhagath.email
+
+fill_in "Password", with: @bhagath.password
+
+click_button "Log in"
+
+expect(page).to have_content("Signed in successfully.")
+
+expect(page).to have_content("Signed in as #{@bhagath.email}")
+
+end
+
+end
